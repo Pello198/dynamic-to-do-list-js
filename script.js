da557a3c0cffc919940722c205ebb6eb63d9ad42
pello198 <kwemoipello@gmail.com>
2025-12-04 17:29:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Step 2: Select DOM elements
-    const addButton = document.getElementById('add-task');
+    const addButton = document.getElementById('add-task-btn');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
